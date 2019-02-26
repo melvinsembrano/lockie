500b6e386c2de6581bca778ad706f6e41397962f
Melvin Sembrano <melvinsembrano@gmail.com>
2019-02-26 13:48:44 +1100

    add controller helper

---
+++ lib/lockie/controller_helper.rb
@@ -0,0 +1,41 @@
+module Lockie
+  module ControllerHelper
+    extend ActiveSupport::Concern
+
+    included do
+      if respond_to?(:helper_method)
+        helper_method :current_user
+        helper_method :logged_in?
+        helper_method :authenticated?
+      end
+
+      def warden
+        return request.env['warden'] if defined?(request)
+        env['warden']
+      end
+
+      def current_user(*args)
+        warden.user(*args)
+      end
+
+      def authenticate!(scope = warden.config.default_scope)
+        warden.authenticate!({ scope: scope }.compact)
+      end
+
+      def authenticate(scope = warden.config.default_scope)
+        warden.authenticate({ scope: scope }.compact)
+      end
+
+      def authenticated?(*args)
+        warden.authenticated?(*args)
+      end
+      alias logged_in? authenticated?
+
+      def logout(*args)
+        authenticated?(args)
+        warden.logout
+      end
+
+    end
+  end
+end
