4c323e0f77914d649d863eb9b15a369a1bba346a
Melvin Sembrano <melv@hey.com>
2022-12-15 20:37:21 +1100

    make FailureApp configurable

--- lib/lockie.rb
+++ lib/lockie.rb
@@ -1,6 +1,7 @@
 require 'rails'
 require 'warden'
 require_relative "lockie/rails"
+require_relative "lockie/failure_app"
 require_relative "lockie/log_helper"
 require_relative "lockie/model_helper"
 require_relative "lockie/controller_helper"
@@ -9,7 +10,6 @@ require_relative "lockie/strategies/jwt"
 require_relative "lockie/strategies/failed"
 
 module Lockie
-  autoload :FailureApp, 'lockie/failure_app'
 
   class Configuration
     attr_accessor :model_name
@@ -22,6 +22,7 @@ module Lockie
     attr_accessor :scopes
     attr_accessor :serializer_to_session, :serializer_from_session
     attr_accessor :session_timeout
+    attr_accessor :failure_app
 
     def initialize
       @model_name = "User"
@@ -34,6 +35,7 @@ module Lockie
       @serializer_to_session = nil
       @serializer_from_session = nil
       @session_timeout = 3.hours
+      @failure_app = Lockie::FailureApp
     end
   end
 
